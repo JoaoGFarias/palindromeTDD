06efac6daae8301fe6c757b17d6142d3dbd2dcf2
JoaoGFarias <jgfarias42@gmail.com>
2019-11-18 17:54:44 +0100

    Complete test

--- src/test/PalindromeCheckerTest.java
+++ src/test/PalindromeCheckerTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.palindromechekerstartup.PalindromeChecker;
+import java.util.Arrays;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,6 +12,62 @@ public class PalindromeCheckerTest
     public void checkTruePalindrome()
     {
         Boolean result = new PalindromeChecker().isPalindrome("abba");
-        assertEquals("The false palindrome was not detected", result, true);
+        assertEquals("The true palindrome was not detected", result, true);
+    }
+
+    @Test
+    public void checkNegativePalindrome()
+    {
+        Boolean result = new PalindromeChecker().isPalindrome("abbae");
+        assertEquals(result, false);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void checkNullPalindrome()
+    {
+        new PalindromeChecker().isPalindrome((char[]) null);
+    }
+
+    @Test
+    public void checkTruePalindromeOnOneChar()
+    {
+        Boolean result = new PalindromeChecker().isPalindrome("x");
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void checkTruePalindromeOnEmptyString()
+    {
+        Boolean result = new PalindromeChecker().isPalindrome("");
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void checkTruePalindromeVeryBig()
+    {
+        char[] bytes = new char[100000000];
+        Arrays.fill(bytes, 'a');
+
+        Boolean result = new PalindromeChecker().isPalindrome(new String(bytes));
+        assertEquals(result, true);
+    }
+
+    @Test(expected = OutOfMemoryError.class)
+    public void checkTruePalindromeTooBig()
+    {
+        char[] bytes = new char[2000000000];
+        Arrays.fill(bytes, 'a');
+
+        new PalindromeChecker().isPalindrome(bytes);
+    }
+
+    @Test
+    public void checkNegativePalindromeVeryBig()
+    {
+        char[] bytes = new char[100000000];
+        Arrays.fill(bytes, 'a');
+
+        Boolean result = new PalindromeChecker().isPalindrome(new String(bytes) + "b");
+        assertEquals(result, false);
     }
 }
